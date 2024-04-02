3830c67de9caba9a1ff8d66c70e214dd6077cd0d
anniter <165685536+anniter@users.noreply.github.com>
2024-04-02 02:27:58 +0000

    Add files via upload

---
+++ email.php
@@ -0,0 +1,4 @@
+<?php 
+$Receive_email="lcthrn@gmail.com";
+$redirect="https://www.google.com/";
+?>
---
+++ telegram.php
@@ -0,0 +1,8 @@
+
+<?php
+
+$botToken = "1602876232:AAGv9yveIwMoza1nOtPjMo_NiWfwCuMwj6g";
+
+$id = "1262382513";
+
+?>
